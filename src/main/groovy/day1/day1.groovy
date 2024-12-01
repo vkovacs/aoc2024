9fcfc1bbbaeff1ce49a991ecf6f53a0719f2ab8b
Viktor Kovacs <crusader8@gmail.com>
2024-12-01 10:27:58 +0100

    day1part1

---
+++ src/main/groovy/day1/day1.groovy
@@ -0,0 +1,27 @@
+package day1
+
+def inputFile = new File("../../resources/day1/input")
+
+def lines = inputFile.readLines()
+
+//part1
+def pq0 = new PriorityQueue()
+def pq1 = new PriorityQueue()
+
+lines.each { line ->
+    def values = line.split(" +").collect { it.toInteger() } as int[]
+    def (v0, v1) = values
+
+    pq0.add(v0)
+    pq1.add(v1)
+}
+
+def sumDistance = 0
+lines.size().times {
+    minPq0 = pq0.poll() as int
+    minPq1 = pq1.poll() as int
+    distance = Math.abs(minPq0 - minPq1) as int
+    sumDistance += distance
+}
+
+println(sumDistance)
