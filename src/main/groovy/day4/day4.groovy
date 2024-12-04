a856daf5218d7bf3ce53fe7c46e1502e2fb48b8c
Viktor Kovacs <crusader8@gmail.com>
2024-12-04 11:50:34 +0100

    day4part2

--- src/main/groovy/day4/day4.groovy
+++ src/main/groovy/day4/day4.groovy
@@ -5,6 +5,7 @@ import groovy.transform.ToString
 
 def inputFile = new File("../../resources/day4/input")
 def testInputFile = new File("../../resources/day4/input-test")
+def testInputFile2 = new File("../../resources/day4/input-test2")
 
 @ToString
 @EqualsAndHashCode
@@ -48,14 +49,14 @@ def walk(matrix, x, y, directions, letterIndexToFind, word, visited) {
     def count = 0
     if (letterIndexToFind < word.size() && currentLetter == word[letterIndexToFind]) {
         if (letterIndexToFind == word.size() - 1) {
-            println(visited << [x,y])
+            //println(visited << [x, y])
             return 1
         }
 
         def nextLetterIndex = ++letterIndexToFind
 
         def neighbours = neighbours(x, y, directions)
-        def newVisited = (visited << [x,y])
+        def newVisited = (visited << [x, y])
         neighbours.each { neighbour ->
             count += walk(matrix, neighbour.x, neighbour.y, directions, nextLetterIndex, word, newVisited)
         }
@@ -76,8 +77,38 @@ def neighbours(int i, int j, directions) {
     neighbours
 }
 
-def part2() {
+def part2(input) {
+    matrix = [][]
+    input.readLines().each {
+        matrix << it.split("")
+    }
 
+    def maxRows = matrix.size()
+    def maxCols = matrix[0].size()
+    xMasOccurrenceCount = 0
+
+    def NW = new Coordinate(-1, -1)
+    def NE = new Coordinate(-1, 1)
+    def SW = new Coordinate(1, -1)
+    def SE = new Coordinate(1, 1)
+
+    def directions = [NW, NE, SW, SE]
+
+    maxRows.times { i ->
+        maxCols.times { j ->
+            if (matrix[i][j] == "A") {
+                if (neighbours(i, j, directions).size() == 4) {
+                    if ((walk(matrix, i + NW.x, j + NW.y, [SE], 0, "MAS", []) > 0 ||
+                            walk(matrix, i + NW.x, j + NW.y, [SE], 0, "SAM", []) > 0) &&
+                            (walk(matrix, i + NE.x, j + NE.y, [SW], 0, "MAS", []) > 0 ||
+                            walk(matrix, i + NE.x, j + NE.y, [SW], 0, "SAM", []) > 0)) {
+                        xMasOccurrenceCount++
+                    }
+                }
+            }
+        }
+    }
+    xMasOccurrenceCount
 }
 
 def testSolution1 = part1(testInputFile, "XMAS")
@@ -87,3 +118,11 @@ testSolution1 == 18
 def solution1 = part1(inputFile, "XMAS")
 println(solution1)
 solution1 == 2633
+
+def testSolution2 = part2(testInputFile2)
+println(testSolution2)
+assert testSolution2 == 9
+
+def solution2 = part2(inputFile)
+println(solution2)
+assert solution2 == 1936
