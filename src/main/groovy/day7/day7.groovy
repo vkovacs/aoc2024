ebf91c3fa2f73a799db9f3828bfe7e8e4adca569
Viktor Kovacs <crusader8@gmail.com>
2024-12-07 09:20:02 +0100

    day7part1

---
+++ src/main/groovy/day7/day7.groovy
@@ -0,0 +1,77 @@
+package day7
+
+def inputFile = new File("../../resources/day7/input")
+def testInputFile = new File("../../resources/day7/input-test")
+
+def read(source) {
+    source.readLines().collect { it.replace(":", "").split(" ")*.toLong() }
+}
+
+def operatorCombinations(operators, length) {
+    def combinations = [""]
+
+    length.times {
+        def tmpCombinations = []
+        operators.each { operator ->
+            combinations.each { combination ->
+                tmpCombinations << combination + operator
+            }
+        }
+        combinations = tmpCombinations
+    }
+    combinations
+}
+
+
+def calculate(operators, numbers) {
+    def tmpNumbers = numbers.collect() as List
+    def tmpOperators = operators.split("") as List
+
+    while (tmpNumbers.size() > 1) {
+        def first = tmpNumbers.remove(0)
+        def second = tmpNumbers.remove(0)
+        def operator = tmpOperators.remove(0)
+
+        def product = switch (operator) {
+            case "+" -> first + second
+            case "*" -> first * second
+        }
+
+        tmpNumbers.add(0, product)
+    }
+
+    tmpNumbers[0]
+}
+
+def part1(input) {
+    def sum = 0
+
+    for (line in input) {
+        def expectedProduct = line.first()
+        def numbers = line.drop(1)
+
+        def operators = operatorCombinations(["+","*"], numbers.size() - 1)
+
+        for (o in operators) {
+            def product = calculate(o, numbers)
+            if (product == expectedProduct) {
+                sum+=product
+                break
+            }
+        }
+    }
+    sum
+}
+
+def part2() {
+
+}
+
+
+def testSolution1 = part1(read(testInputFile))
+println(testSolution1)
+assert testSolution1 == 3749
+
+def solution1 = part1(read(inputFile))
+println(solution1)
+asset solution1 == 3351424677624
