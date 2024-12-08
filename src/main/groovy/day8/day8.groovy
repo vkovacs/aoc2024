258bebacf2ec799585f303e4a47482e3895a9d79
Viktor Kovacs <crusader8@gmail.com>
2024-12-08 13:53:32 +0100

    day8part2

--- src/main/groovy/day8/day8.groovy
+++ src/main/groovy/day8/day8.groovy
@@ -5,6 +5,7 @@ import groovy.transform.ToString
 
 def inputFile = new File("../../resources/day8/input")
 def testInputFile = new File("../../resources/day8/input-test")
+def testInputFile2 = new File("../../resources/day8/input-test2")
 
 @ToString
 @EqualsAndHashCode
@@ -59,7 +60,7 @@ def part1(map, coordinatesMap) {
             for (j in (0..<v.size())) {
                 if (i != j) {
                     def (dx, dy) = d(v[i], v[j])
-                    println(v[i].toString() + " " + v[j].toString() + " " + dx + " " + dy)
+                    //println(v[i].toString() + " " + v[j].toString() + " " + dx + " " + dy)
 
                     n0x = v[i].x + dx
                     n0y = v[i].y + dy
@@ -79,20 +80,64 @@ def part1(map, coordinatesMap) {
         }
     }
 
+    printMap(map, nodeCoordinates)
+
+    return nodeCoordinates.size()
+}
+
+def part2(map, coordinatesMap) {
+    def nodeCoordinates = [] as Set
+
+    coordinatesMap.each { k, v ->
+        for (i in (0..<v.size())) {
+            for (j in (0..<v.size())) {
+                if (i != j) {
+                    def (dx, dy) = d(v[i], v[j])
+                    //println(v[i].toString() + " " + v[j].toString() + " " + dx + " " + dy)
+                    nodeCoordinates << new Coordinate(v[i].x, v[i].y)
+                    nodeCoordinates << new Coordinate(v[j].x, v[j].y)
+
+                    n0x = v[i].x
+                    n0y = v[i].y
+                    do {
+                        n0x = n0x + dx
+                        n0y = n0y + dy
+
+                        if (inRange(map, n0x, n0y)) {
+                            nodeCoordinates << new Coordinate(n0x, n0y)
+                        }
+                    } while (inRange(map, n0x, n0y))
+
+                    n1x = v[i].x
+                    n1y = v[i].y
+
+                    do {
+                        n1x = n1x - 2 * dx
+                        n1y = n1y - 2 * dy
+
+                        if (inRange(map, n1x, n1y)) {
+                            nodeCoordinates << new Coordinate(n1x, n1y)
+                        }
+                    } while (inRange(map, n1x, n1y))
+                }
+            }
+        }
+    }
+
+    printMap(map, nodeCoordinates)
+
+    return nodeCoordinates.size()
+}
+
+private void printMap(map, Set nodeCoordinates) {
     for (i in (0..<map.size())) {
         for (j in (0..<map.size())) {
-            if (new Coordinate(i,j) in nodeCoordinates) {
+            if (new Coordinate(i, j) in nodeCoordinates) {
                 print("#")
             } else print(".")
         }
         println()
     }
-
-    return nodeCoordinates.size()
-}
-
-def part2() {
-
 }
 
 def testSolution1 = part1(*read(testInputFile))
@@ -102,3 +147,11 @@ assert testSolution1 == 14
 def solution1 = part1(*read(inputFile))
 println(solution1)
 assert solution1 == 293
+
+def testSolution2 = part2(*read(testInputFile2))
+println(testSolution2)
+assert testSolution2 == 9
+
+def solution2 = part2(*read(inputFile))
+println(solution2)
+assert solution2 == 934
