a31f4fe3436c37ebd63f92f689139b274f9039b1
Viktor Kovacs <crusader8@gmail.com>
2024-12-11 13:00:58 +0100

    day10part1

---
+++ src/main/groovy/day10/day10.groovy
@@ -0,0 +1,63 @@
+package day10
+
+def inputFile = new File("../../resources/day10/input")
+def testInputFile = new File("../../resources/day10/input-test")
+
+
+def read(input) {
+    def map = []
+    input.readLines().each { line ->
+        map << line.split("")*.toInteger()
+    }
+    map
+}
+
+def neighbours(def matrix, int i, int j, directions) {
+    def neighbours = []
+    directions.each { direction ->
+        neighbourX = i + direction[0]
+        neighbourY = j + direction[1]
+        if (neighbourX >= 0 && neighbourX < matrix.size() && neighbourY >= 0 && neighbourY < matrix[i].size()) {
+            neighbours << [neighbourX, neighbourY]
+        }
+    }
+    neighbours
+}
+
+def score(map, i, j, currentHeight, visited) {
+    if (currentHeight == 9) return 1
+
+    def directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+    def validNeighbours = neighbours(map, i, j, directions).findAll { n -> map[n[0]][n[1]] == currentHeight + 1 && !visited.contains([n[0], n[1]]) }
+    if (validNeighbours.isEmpty()) return 0
+
+    def count = 0
+    for (neighbour in validNeighbours) {
+        visited << [neighbour[0], neighbour[1]]
+        count += score(map, neighbour[0], neighbour[1], currentHeight + 1, visited)
+    }
+
+    count
+}
+
+def part1(map) {
+    //println(map)
+    def sumScore = 0
+    for (i in (0..<map.size())) {
+        for (j in (0..map[i].size())) {
+            if (map[i][j] == 0) {
+                sumScore += score(map, i, j, 0, [] as Set)
+            }
+        }
+    }
+    sumScore
+}
+
+def part2() {
+
+}
+//def m = read(testInputFile)
+//println score(m, 0, 2, 0, [] as Set)
+
+assert part1(read(testInputFile)) == 36
+assert part1(read(inputFile)) == 607
