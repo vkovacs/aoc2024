ffff00ea914697a093ce573da4462b24dd4ba65c
Viktor Kovacs <crusader8@gmail.com>
2024-12-11 12:07:05 +0100

    day9part2

--- src/main/groovy/day9/day9.groovy
+++ src/main/groovy/day9/day9.groovy
@@ -46,7 +46,9 @@ BigInteger checksum(List<Integer> orderedBlocks) {
     BigInteger sum = 0
     def i = 0
     while (i < orderedBlocks.size()) {
-        sum = sum + (orderedBlocks[i] * i)
+        if (orderedBlocks[i] > -1) {
+            sum = sum + (orderedBlocks[i] * i)
+        }
         i++
     }
 
@@ -62,3 +64,78 @@ def part1(input) {
 def input = inputFile.readLines()[0]
 
 assert part1(input) == 6378826667552
+
+/part2/
+
+def ranges(list) {
+    if (list.isEmpty()) return []
+
+    def sublists = [] as LinkedList
+    def currentSublist = [list[0]]
+
+    list.tail().each { num ->
+        if (num == currentSublist.last() as int) {
+            currentSublist << num
+        } else {
+            sublists << currentSublist
+            currentSublist = [num]
+        }
+    }
+
+    sublists << currentSublist // Add the last sublist
+    return sublists
+}
+
+def swap(ranges, i, j) {
+    if (ranges[i].size() == ranges[j].size()) {
+        def tmp = ranges[i]
+        ranges[i] = ranges[j]
+        ranges[j] = tmp
+        return false
+    } else if (ranges[j].size() < ranges[i].size()) {
+        int sizeDiff = ranges[i].size() - ranges[j].size()
+        def buffer = new LinkedList<>(Collections.nCopies(sizeDiff, -1))
+
+        def tmp = ranges[i]
+        ranges[i] = ranges[j]
+        ranges.add(i + 1, buffer)
+        ranges[j + 1] = new LinkedList<>(Collections.nCopies(tmp.size() - sizeDiff, -1))
+        return true
+    }
+}
+
+
+def part2(input) {
+    def blocks = inputBlocks(input)
+    //println(String.valueOf(blocks.join()).replace("-1", "."))
+    def r = ranges(blocks)
+    def (i, j) = [0, r.size() - 1]
+    while (i < j) {
+//        println("$i $j")
+//        println(r)
+        if (i % 100 == 0) println(i)
+        if (r[i][0] != -1) {
+            i++
+            continue
+        } else if (r[j][0] == -1) {
+            j--
+            continue
+        }
+        //println(String.valueOf(r.flatten().join()).replace("-1", "."))
+        int k = i
+        while (k < j) {
+            if (r[k][0] == -1 && r[j][0] != -1 && r[j].size() <= r[k].size()) {
+                def increaseJ = swap(r, k, j)
+                if(increaseJ) j++
+                break
+            }
+            k++
+        }
+        j--
+    }
+
+    checksum(r.flatten())
+}
+
+assert part2("2333133121414131402") == 2858
+assert part2(inputFile.readLines()[0]) == 6413328569890
